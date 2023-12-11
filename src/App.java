05ca77769d92333cbdbf0d9d9d942c3f1679ff2f
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-12-11 09:02:05 +0100

    uppg a klar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tb = new Scanner(System.in);
+        System.out.println("VadeDittAnvändarnamn?");
+        String user = tb.nextLine();
+        System.out.println("Vadärlösenordet?");
+        String pass = tb.nextLine();
+        if (user.equals("root") && pass.equals("pass")) {
+            System.out.println("BROKIE!! Du har 0 kr");
+        }
+        else{
+            System.out.println("Fel användarnamn eler lösenord");
+        }
     }
 }
