3fabf35ccfbf9f9eaa872d2b12241d6e5f4a2b28
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-12-11 09:05:39 +0100

    Uppg b kar

--- src/App.java
+++ src/App.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tb = new Scanner(System.in);
-        System.out.println("VadeDittAnvändarnamn?");
-        String user = tb.nextLine();
-        System.out.println("Vadärlösenordet?");
-        String pass = tb.nextLine();
-        if (user.equals("root") && pass.equals("pass")) {
-            System.out.println("BROKIE!! Du har 0 kr");
+        System.out.println("DroppaEttTal");
+        int tal = tb.nextInt();
+        if (tal<=10) {
+            System.out.println(tal+" är mindre än eller lika med 10");
         }
-        else{
-            System.out.println("Fel användarnamn eler lösenord");
+        else if (tal>10){
+            System.out.println(tal+" är större än 10");
         }
     }
 }
